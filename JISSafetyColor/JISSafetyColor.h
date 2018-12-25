beed31f9811ac767e4cb428eafdda652fd20c7c2
Hidemune Takahashi <hidemune.takahashi@outlook.com>
2018-12-25 21:42:22 +0900

    Added Xcode Project.

---
+++ JISSafetyColor/JISSafetyColor.h
@@ -0,0 +1,19 @@
+//
+//  JISSafetyColor.h
+//  JISSafetyColor
+//
+//  Created by hidemune on 12/25/18.
+//  Copyright © 2018 Hidemune Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JISSafetyColor.
+FOUNDATION_EXPORT double JISSafetyColorVersionNumber;
+
+//! Project version string for JISSafetyColor.
+FOUNDATION_EXPORT const unsigned char JISSafetyColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JISSafetyColor/PublicHeader.h>
+
+
